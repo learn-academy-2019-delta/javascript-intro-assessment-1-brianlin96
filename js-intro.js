66700fd5c7cc05a2bb6ef2b791b6d844ffe39b57
Ubuntu <ubuntu@ip-172-31-45-116.us-east-2.compute.internal>
2019-08-02 22:28:00 +0000

    completed assessment

--- js-intro.js
+++ js-intro.js
@@ -1,62 +1,138 @@
-// ASSESSMENT 1: INTRO TO JAVASCRIPT
-// Coding practical questions
+//// ASSESSMENT 1: INTRO TO JAVASCRIPT
+//// Coding practical questions
 
-// ---------------------------------------------
-// Consider this variable:
+//// ---------------------------------------------
+//// Consider this variable:
 
-var mantra = "Be the dev"
+//var mantra = "Be the dev"
 
-// 1a. Write the code that returns 'B' from mantra.
+//// 1a. Write the code that returns 'B' from mantra.
+//console.log(mantra[0])
+//// 1b. Write the code that determines if there is an 'x' in mantra.
+// let checkLetter = (str, char) => {
+//     if(str.indexOf(char)>0){
+//         console.log(`There is a ${char} in "${str}"`)
+//     } else {
+//         console.log(`${char} is not in "${str}"`)
+//     }
+// }
+//// calling function with 'x'
+// checkLetter(mantra, 'x')
+//// 1c. Write the code that determines if there is a 'v' in mantra.
+// let checkLetter = (str, char) => {
+//     if(str.indexOf(char)>0){
+//         console.log(`There is a ${char} in "${str}"`)
+//     } else {
+//         console.log(`${char} is not in "${str}"`)
+//     }
+// }
+//// calling funtions with 'v'
+// checkLetter(mantra, 'v')
+//// 1d. Stretch: Write a function that can take any letter as an argument and returns if it exists within the given string.
+// let checkLetter = (str, char) => {
+//     if(str.indexOf(char)>0){
+//         console.log(`There is a ${char} in "${str}"`)
+//     } else {
+//         console.log(`${char} is not in "${str}"`)
+//     }
+// }
 
-// 1b. Write the code that determines if there is an 'x' in mantra.
+//// ---------------------------------------------------
+//// Consider the following variables:
 
-// 1c. Write the code that determines if there is a 'v' in mantra.
+// var myDog = "Snoopy"
+// var myCat = "Garfield"
 
-// 1d. Stretch: Write a function that can take any letter as an argument and returns if it exists within the given string.
-
-
-// ---------------------------------------------------
-// Consider the following variables:
-
-var myDog = "Snoopy"
-var myCat = "Garfield"
-
-// 2a. Create a function that returns the longer of the two variables. Expected output: "Garfield"
-
-// 2b. Create a function that takes in two arguments and returns an array containing the two variables. Expected output: ["Snoopy", "Garfield"]
-
-// 2c. Stretch: Write a function that returns the two variables in all lowercase letters.
-
-
-// -----------------------------------------------
-// Consider this variable:
+// // 2a. Create a function that returns the longer of the two variables. Expected output: "Garfield"
+// let checkWord = (str1, str2) => {
+//     return (str1.length>str2.length ? `${str1} is longer than ${str2}`:`${str2} is longer than ${str1}`)
+// }
+// console.log(checkWord(myDog, myCat))
+// // 2b. Create a function that takes in two arguments and returns an array containing the two variables. Expected output: ["Snoopy", "Garfield"]
+// let makeFunc = (str1, str2) => {
+//     let arr = []
+//     arr.push(str1, str2)
+//     return arr
+// }
+// console.log(makeFunc(myDog, myCat))
+// // 2c. Stretch: Write a function that returns the two variables in all lowercase letters.
+// let makeLowerCase = (str1, str2) => {
+//     return `${str1.toLowerCase()}\n${str2.toLowerCase()}`
+// }
+// console.log(makeLowerCase(myDog, myCat))
+//// -----------------------------------------------
+//// Consider this variable:
 
 var myMessage = "Learning is fun!"
 
 // 3a. Write the code that logs each letter of the message using a for loop.
+for(let i = 0; i<myMessage.length; i++){
+    console.log(myMessage[i])
+}
 
 // 4b. Stretch: Write the code that logs each letter of the message using a while loop.
-
+var i = 0
+while(i<myMessage.length){
+    console.log(myMessage[i])
+    i++
+}
 // 4c. Super Stretch: Write the code that logs each letter of the message using forEach().
+myMessage.split('').forEach(x=>console.log(x))
 
+//// ----------------------------------------------
+//// Consider the following variable:
 
-// ----------------------------------------------
-// Consider the following variable:
+// var testString = "thisisateststring"
 
-var testString = "thisisateststring"
+// // 5a. Write a function that takes the variable as an argument and returns the string without vowels. Expected output: "thsstststrng"
+// let remVowels = (str) => {
+//     return str.replace(/[aeiou]/g, '')
+// }
+// console.log(remVowels(testString))
 
-// 5a. Write a function that takes the variable as an argument and returns the string without vowels. Expected output: "thsstststrng"
+// // 5b. Stretch: Update your function to return an error message if the variable is not a string. Copy and paste your function to add these changes.
+// let remVowel = (str) => {
+//     if(typeOf str !== "string"){
+//         return "This is not a string"
+//     } else {
+//     return str.replace(/[aeiou]/g, '')
+//     }
+// }
+// console.log(remVowels(testString))
 
-// 5b. Stretch: Update your function to return an error message if the variable is not a string. Copy and paste your function to add these changes.
+// // ----------------------------------------------
+// // Consider the following variable:
 
+// var toonimals = [ {name: "Itchy", animal: "mouse"}, {name: "Stimpy", animal: "cat"}, {name: "Daffy", animal: "duck"}, {name: "Scratchy", animal: "cat"}, {name: "Ren", animal: "dog"}, {name: "Felix", animal: "cat"}]
 
-// ----------------------------------------------
-// Consider the following variable:
-
-var toonimals = [ {name: "Itchy", animal: "mouse"}, {name: "Stimpy", animal: "cat"}, {name: "Daffy", animal: "duck"}, {name: "Scratchy", animal: "cat"}, {name: "Ren", animal: "dog"}, {name: "Felix", animal: "cat"}]
-
-// 6a. Write a function that takes in the toonimals array as an argument and returns an array with only the toon objects that are cats. Expected output: [ { name: "Stimpy", animal: "cat" }, { name: "Scratchy", animal: "cat" }, { name: "Felix", animal: "cat" } ]
-
-//6b. Write a function that returns only the names of all the non-cats. Expected output: "Itchy" "Daffy" "Ren"
-
-//6c. Stretch: Create a function that will print a sentence for every toon stating their name and their animal type. Use string interpolation. Expected output: "Itchy is a mouse" "Stimpy is a cat" "Daffy is a duck" "Scratchy is a cat" "Ren is a dog" "Felix is a cat"
+// // 6a. Write a function that takes in the toonimals array as an argument and returns an array with only the toon objects that are cats. Expected output: [ { name: "Stimpy", animal: "cat" }, { name: "Scratchy", animal: "cat" }, { name: "Felix", animal: "cat" } ]
+// let ifCat = (listOfToonimals) => {
+//     let arr = []
+//     listOfToonimals.forEach(function (toonimal){
+//         if(toonimal.animal=="cat"){
+//             arr.push(toonimal)
+//         }
+//     })
+    
+//     return arr
+// }
+// console.log(ifCat(toonimals))
+// //6b. Write a function that returns only the names of all the non-cats. Expected output: "Itchy" "Daffy" "Ren"
+// let ifNotCat = (listOfToonimals) => {
+//     let arr = []
+//     listOfToonimals.forEach(function (toonimal){
+//         if(toonimal.animal!="cat"){
+//             arr.push(toonimal.name)
+//         }
+//     })
+    
+//     return arr
+// }
+// console.log(ifNotCat(toonimals))
+// //6c. Stretch: Create a function that will print a sentence for every toon stating their name and their animal type. Use string interpolation. Expected output: "Itchy is a mouse" "Stimpy is a cat" "Daffy is a duck" "Scratchy is a cat" "Ren is a dog" "Felix is a cat"
+// let getToonimalData = (listOfToonimals) => {
+//     listOfToonimals.forEach(function (toonimal){
+//         console.log(`${toonimal.name} is a ${toonimal.animal}`)
+//     })
+// }
+// getToonimalData(toonimals)
--- js-intro.md
+++ js-intro.md
@@ -5,69 +5,69 @@ Answer the following questions. First, without external resources. Challenge you
 
 1. What is a function? Why would you use one?
 
-  Your answer:
+  Your answer: Object that is used to complete some sort of action 
 
-  Researched answer:
+  Researched answer: A JavaScript function is a block of code designed to perform a particular task.
 
 
 2. What is a method?
 
-  Your answer:
+  Your answer: function that acts on an object 
 
-  Researched answer:
+  Researched answer: JavaScript methods are actions that can be performed on objects.
 
 
 3. List 5 examples of JavaScript string methods.
 
-  Your answer:
+  Your answer: str.length() , str.indexOf(), str.split(""), str.charAt(1), str.toUpperCase
 
-  Researched answer:
+  Researched answer: str.charAt(), str.repeat(), str.replace(), str.search(), str.slice()
 
 
 4. List 5 examples of JavaScript array methods.
 
-  Your answer:
+  Your answer: arr.length, arr.pop, arr1.concat(arr2), arr.concat, arr.push()
 
-  Researched answer:
+  Researched answer: arr.shift(), arr.unshift("str"), arr.map(function()), arr.reverse(), arr.splice()
 
 
 5. Explain the difference between accessor and mutator methods. Provide an example of each.
 
-  Your answer:
+  Your answer: accessor gets information from an array without changing it. The mutators act on the arrays and will modify them. Accessor: arr.join(""). Mutator: arr.push("hi")
 
-  Researched answer:
+  Researched answer: Methods that modify the original array are known as mutator methods, and methods that return a new value or representation are known as accessor methods. Accessor: arr.indexOf(5). Mutator. arr.reverse()
 
 
 6. Explain the difference between console.log() and return.
 
-  Your answer:
+  Your answer: console.log() prints out whatever is within the parentheses onto the console. Return will return a value to the function or method that can be accessed by other functions/methods
 
-  Researched answer:
+  Researched answer: The Console method log() outputs a message to the web console. The return statement ends function execution and specifies a value to be returned to the function caller.
 
 
 7. In regards to functions, what is an argument? What is the difference between an argument and a variable?
 
-  Your answer:
+  Your answer: An argument is the variable that the function is going to be acting on. A variable is something that hold information
 
-  Researched answer:
+  Researched answer: Arguments are values passed to the function when it is invoked. JavaScript variables are containers for storing data values.
 
 
 8. Give a brief description of JavaScript as if you were explaining it to someone who doesn't know anything about programming.
 
-  Your answer:
+  Your answer: Object-oriented langauge that is used for front-end development
 
-  Researched answer:
+  Researched answer: JavaScript, often abbreviated as JS, is a high-level, interpreted scripting language that conforms to the ECMAScript specification. JavaScript has curly-bracket syntax, dynamic typing, prototype-based object-orientation, and first-class functions
 
 
 9. Give a brief description of proper pair programming techniques. What are the rolls of each person?
 
-  Your answer:
+  Your answer: In pair programming, there is a driver and a navigator. The driver is the one that is writing out the code while the navigator is the one helping to direct through looking for errors, and helping think through the logic 
 
-  Researched answer:
+  Researched answer: Pair programming is an agile software development technique in which two programmers work together at one workstation. One, the driver, writes code while the other, the observer or navigator, reviews each line of code as it is typed in. The two programmers switch roles frequently.
 
 
 10. What is one thing we did in class this week you found helpful?  
 
-  Your answer:
+  Your answer: The super stretch challenges 
 
-  Researched answer:
+  Researched answer: ?
